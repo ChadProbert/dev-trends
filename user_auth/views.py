12c61ec883ee5c9c373115b0d467bcc27751f3a2
Chad Probert <chadcprobert@gmail.com>
2024-08-10 13:45:06 +0200

    Added docstrings to views.py

--- user_auth/views.py
+++ user_auth/views.py
@@ -6,62 +6,54 @@ from django.contrib.auth.models import User
 
 
 def user_login(request):
-    """
-    purpose:
-    - to render the login page
+    """Render the login page.
 
-    args:
-    - HttpRequest object
+    :param HttpRequest request: The HTTP request object.
 
-    returns:
-    - rendered login.html template
+    :returns: The rendered 'login.html' template.
+    :rtype: HttpResponse
     """
     return render(request, "authentication/login.html")
 
 
 def authenticate_user(request):
-    """
-    purpose:
-    - to authenticate the user based on credentials given
+    """Authenticate the user based on provided credentials.
 
-    args:
-    - HttpRequest object containing post request data (username and password)
+    :param HttpRequest request: The HTTP request object containing POST data.
 
-    returns:
-    - HttpResponseRedirect object if successful
-    - re-renders login.html template with error message if unsuccessful
+    :returns: A redirect to the 'index' page if successful,
+              otherwise re-renders 'login.html' with an error message.
+    :rtype: HttpResponse or HttpResponseRedirect
     """
 
-    # captures & attempts to authenticate the credentials provided by the user
+    # Captures & attempts to authenticate the credentials provided by the user
     username = request.POST["username"]
     password = request.POST["password"]
     user = authenticate(username=username, password=password)
 
-    # if the login object returns None, then the user is not authenticated
+    # If the login object returns None, then the user is not authenticated
     if user is None:
-        # added error handling for if the login credentials are incorrect
+        # Added error handling for if the login credentials are incorrect
         error_message = "Invalid username or password."
         return render(
             request, "authentication/login.html", {"error_message": error_message}
         )
 
-    # if the login object returns a user object, then the user is authenticated and redirected
+    # If the login object returns a user object, then the user is authenticated
+    # and redirected
     else:
         login(request, user)
-        # directs the user to the index page '/'
+        # Directs the user to the index page '/'
         return HttpResponseRedirect(reverse("news:index"))
 
 
 def show_user(request):
-    """
-    purpose:
-    - to display the authenticated user's information
+    """Display the authenticated user's information.
 
-    args:
-    - HttpRequest object
+    :param HttpRequest request: The HTTP request object.
 
-    Returns:
-    - rendered user.html template with the user's username and password
+    :returns: The rendered 'user.html' template with the user's username and password.
+    :rtype: HttpResponse
     """
     return render(
         request,
@@ -71,26 +63,22 @@ def show_user(request):
 
 
 def register(request):
-    """
-    purpose:
-    - to register a new user
+    """Register a new user and log them in if successful.
 
-    args:
-    - HttpRequest object
+    :param HttpRequest request: The HTTP request object.
 
-    returns:
-    - HttpResponseRedirect object if successful
-    - re-renders register.html template with error message if unsuccessful
-    - if the request method is not a POST request, it renders the registration form page
+    :returns: A redirect to the 'index' page if successful,
+            otherwise re-renders 'register.html' with an error message.
+    :rtype: HttpResponse or HttpResponseRedirect
     """
-    # checks if the request method is a POST request
+    # Checks if the request method is a POST request
     if request.method == "POST":
         name = request.POST.get("name")
         username = request.POST.get("username")
         password = request.POST.get("password")
         confirmed_password = request.POST.get("confirmed_password")
 
-        # checks if the passwords match
+        # Checks if the passwords match
         if password != confirmed_password:
             error_message = "Passwords do not match"
             return render(
@@ -99,7 +87,7 @@ def register(request):
                 {"error_message": error_message},
             )
 
-        # checks if the username already exists
+        # Checks if the username already exists
         if User.objects.filter(username=username).exists():
             error_message = "Username already exists"
             return render(
@@ -108,38 +96,36 @@ def register(request):
                 {"error_message": error_message},
             )
 
-        # if the above checks pass, create the user in the db
+        # If the above checks pass, create the user in the db
         user = User.objects.create_user(username=username, password=password)
         user.first_name = name
         user.save()
 
-        # logs the user in if the registration is successful
+        # Logs the user in if the registration is successful
         user = authenticate(username=username, password=password)
         login(request, user)
 
-        # sends the user to the logged in confirmation page
+        # Sends the user to the app
         return HttpResponseRedirect(reverse("news:index"))
 
     return render(request, "authentication/register.html")
 
 
 def user_logout(request):
-    """
-    purpose:
-    - to log the user out
+    """Log the user out and redirect to the login page.
 
-    args:
-    - HttpRequest object
+    :param HttpRequest request: The HTTP request object.
 
-    returns:
-    - user no longer authenticated and is then redirected to the login page
+    :returns: A redirect to the login page.
+    :rtype: HttpResponseRedirect
     """
-    # logs the user out using the built-in logout function
+
+    # Logs the user out using the built-in logout function
     logout(request)
-    # sends the user to the login page
     return redirect("user_auth:login")
 
 
-# references:
+# References:
 # My own work from Level 2 Task 22
-# Docstrings - https://www.youtube.com/watch?v=HsHJo8RRkn0&ab_channel=VeryAcademy
+# Docstrings -
+# https://www.youtube.com/watch?v=HsHJo8RRkn0&ab_channel=VeryAcademy
